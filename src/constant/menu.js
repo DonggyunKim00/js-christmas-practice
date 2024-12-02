d07af697e959795858d91dc21a8df416fa93433d
donggyun <donggyunkim12@gmail.com>
2024-12-02 15:11:21 +0900

    feat(constant): 메뉴 상수 구현

---
+++ src/constant/menu.js
@@ -0,0 +1,50 @@
+export const MENU = Object.freeze({
+  양송이수프: {
+    category: '에피타이저',
+    price: 6000,
+  },
+  타파스: {
+    category: '에피타이저',
+    price: 5500,
+  },
+  시저샐러드: {
+    category: '에피타이저',
+    price: 8000,
+  },
+  티본스테이크: {
+    category: '메인',
+    price: 55000,
+  },
+  바비큐립: {
+    category: '메인',
+    price: 54000,
+  },
+  해산물파스타: {
+    category: '메인',
+    price: 35000,
+  },
+  크리스마스파스타: {
+    category: '메인',
+    price: 25000,
+  },
+  초코케이크: {
+    category: '디저트',
+    price: 15000,
+  },
+  아이스크림: {
+    category: '디저트',
+    price: 5000,
+  },
+  제로콜라: {
+    category: '음료',
+    price: 3000,
+  },
+  레드와인: {
+    category: '음료',
+    price: 60000,
+  },
+  샴페인: {
+    category: '음료',
+    price: 25000,
+  },
+});
