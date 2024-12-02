20e8f0a5f90759e7edaf3451b182ff4bac26710e
donggyun <donggyunkim12@gmail.com>
2024-12-02 15:15:35 +0900

    feat(OutputView): Intro 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { readAndValidateDate, readAndValidateMenu } from './utils/readInput.js';
+import OutputView from './view/OutputView.js';
 
 class App {
   async run() {
+    OutputView.printIntro();
+
     const date = await readAndValidateDate();
     const menuList = await readAndValidateMenu();
+
+    OutputView.printPreview(date);
   }
 }
 
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,7 +3,7 @@ import { Console } from '@woowacourse/mission-utils';
 const OutputView = {
   OUTPUT_MESSAGE: Object.freeze({
     WELCOME_INTRO: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
-    PREVIEW_INTRO: '12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n',
+    PREVIEW_INTRO: (date) => `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
     BEFORE_DISCOUNT_TOTAL_AMOUNT: (amount) => `<할인 전 총주문 금액>\n${amount}원`,
     GIVING_MENU: (menu) => `<증정 메뉴>\n${menu ?? '없음'}`,
     EVENT_LOG: (events) => {
@@ -25,8 +25,8 @@ const OutputView = {
     Console.print(this.OUTPUT_MESSAGE.WELCOME_INTRO);
   },
 
-  printPreview() {
-    Console.print(this.OUTPUT_MESSAGE.PREVIEW_INTRO);
+  printPreview(date) {
+    Console.print(this.OUTPUT_MESSAGE.PREVIEW_INTRO(date));
   },
 
   printError(message) {
