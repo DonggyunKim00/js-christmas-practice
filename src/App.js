22193f99e8e2592d5677441118462b0709544222
donggyun <donggyunkim12@gmail.com>
2024-12-03 09:07:42 +0900

    feat(App,OutputView): preview 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { readAndValidateDate, readAndValidateMenu } from './utils/readInput.js';
 import OutputView from './view/OutputView.js';
 import Menu from './model/Menu.js';
 import MenuList from './model/MenuList.js';
+import EventPlanner from './model/EventPlanner.js';
 
 class App {
   #menuList;
+
+  #eventPlanner;
 
   async run() {
     OutputView.printIntro();
@@ -12,17 +15,59 @@ class App {
     const date = await readAndValidateDate();
     const menus = await readAndValidateMenu();
     this.#createMenuList(menus);
+    this.#createEventPlanner(date);
 
-    OutputView.printPreview(date);
+    this.#displayOrderPreview(date);
   }
 
   #createMenuList(menus) {
-    const orderMenu = menus.split(',').forEach((item) => {
+    const orderMenu = menus.split(',').map((item) => {
       const [name, quantity] = item.split('-');
       return { menu: Menu.create(name), quantity: Number(quantity) };
     });
 
     this.#menuList = new MenuList(orderMenu);
+  }
+
+  #createEventPlanner(date) {
+    this.#eventPlanner = new EventPlanner(date, this.#menuList.getTotalAmount());
+  }
+
+  #displayOrderPreview(date) {
+    OutputView.printPreview(date);
+
+    OutputView.printOrderMenus(this.#menuList.getOrderMenu());
+    OutputView.printPrevDiscountAmount(this.#menuList.getTotalAmount());
+    OutputView.printGivingItem(this.#eventPlanner.getGivingItem());
+    const events = this.#makeApplyEventObject();
+    OutputView.printEventLog(events);
+
+    const amount = Object.entries(events).reduce((acc, cur) => acc + cur[1], 0);
+    OutputView.printEventTotalAmount(amount);
+
+    const amountDiscount = Object.entries(events).reduce((acc, cur, idx) => {
+      if (idx === 4) return acc;
+      return acc - cur[1];
+    }, this.#menuList.getTotalAmount());
+    OutputView.printExpectPayAmount(amountDiscount);
+
+    OutputView.printBadge(this.#eventPlanner.getEventBadge(amount));
+  }
+
+  #makeApplyEventObject() {
+    return {
+      '크리스마스 디데이 할인': this.#eventPlanner.calculateDdayDiscount(),
+      '평일 할인': this.#eventPlanner.calculateWeekdayDiscount(
+        this.#menuList.getMenuCountWithCategory('디저트'),
+      ),
+      '주말 할인': this.#eventPlanner.calculateHolidayDiscount(
+        this.#menuList.getMenuCountWithCategory('메인'),
+      ),
+      '특별 할인': this.#eventPlanner.calculateSpecialdayDiscount(),
+      '증정 이벤트': this.#eventPlanner.getGivingItem()
+        ? this.#eventPlanner.getGivingItem().getInfo().price
+        : 0,
+    };
   }
 }
 
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,22 +3,18 @@ import { Console } from '@woowacourse/mission-utils';
 const OutputView = {
   OUTPUT_MESSAGE: Object.freeze({
     WELCOME_INTRO: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
-    PREVIEW_INTRO: (date) => `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
-    BEFORE_DISCOUNT_TOTAL_AMOUNT: (amount) => `<할인 전 총주문 금액>\n${amount}원`,
-    GIVING_MENU: (menu) => `<증정 메뉴>\n${menu ?? '없음'}`,
-    EVENT_LOG: (events) => {
-      if (!events) return `<혜택 내역>\n없음`;
-      const stringEvent = Object.entries(events)
-        .map(([eventName, money]) => `${eventName}: -${money}`)
-        .join('\n');
-      return `<혜택 내역>\n${stringEvent}`;
-    },
-    EVENT_TOTAL_AMOUNT: (amount) => `<총혜택 금액>\n${amount ? `-${amount}` : '0'}원`,
-    EVENT_BADGE: (badge) => `<12월 이벤트 배지>\n${badge ?? '없음'}`,
+    PREVIEW_INTRO: (date) => `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
+    ORDER_MENU: (menus) => `\n<주문 메뉴>\n${menus}`,
+    BEFORE_DISCOUNT_TOTAL_AMOUNT: (amount) => `\n<할인 전 총주문 금액>\n${amount}원`,
+    GIVING_MENU: (menu) => `\n<증정 메뉴>\n${menu}`,
+    EVENT_LOG: (events) => `\n<혜택 내역>\n${events}`,
+    EVENT_TOTAL_AMOUNT: (amount) => `\n<총혜택 금액>\n${amount}원`,
+    AFTER_DISCOUNT_TOTAL_AMOUNT: (amount) => `\n<할인 후 예상 결제 금액>\n${amount}원`,
+    EVENT_BADGE: (badge) => `\n<12월 이벤트 배지>\n${badge ?? '없음'}`,
   }),
 
-  printBlankLine() {
-    Console.print('');
+  printError(message) {
+    Console.print(`[ERROR] ${message}`);
   },
 
   printIntro() {
@@ -29,8 +25,46 @@ const OutputView = {
     Console.print(this.OUTPUT_MESSAGE.PREVIEW_INTRO(date));
   },
 
-  printError(message) {
-    Console.print(`[ERROR] ${message}`);
+  printOrderMenus(menus) {
+    const stringifyMenus = menus.map((item) => `${item.name} ${item.quantity}개`).join('\n');
+
+    Console.print(this.OUTPUT_MESSAGE.ORDER_MENU(stringifyMenus));
+  },
+
+  printPrevDiscountAmount(amount) {
+    Console.print(this.OUTPUT_MESSAGE.BEFORE_DISCOUNT_TOTAL_AMOUNT(amount.toLocaleString('ko-KR')));
+  },
+
+  printGivingItem(item) {
+    let stringItem = '없음';
+    if (item) stringItem = `${item.getInfo().name} 1개`;
+
+    Console.print(this.OUTPUT_MESSAGE.GIVING_MENU(stringItem));
+  },
+
+  printEventLog(events) {
+    const stringEvent = Object.entries(events)
+      .map(([eventName, money]) => {
+        if (!money) return '';
+        return `${eventName}: -${money.toLocaleString('ko-KR')}원`;
+      })
+      .filter((item) => item !== '')
+      .join('\n');
+
+    Console.print(this.OUTPUT_MESSAGE.EVENT_LOG(stringEvent || '없음'));
+  },
+
+  printEventTotalAmount(amount) {
+    const strAmount = amount > 0 ? `-${amount.toLocaleString('ko-KR')}` : 0;
+    Console.print(this.OUTPUT_MESSAGE.EVENT_TOTAL_AMOUNT(strAmount));
+  },
+
+  printExpectPayAmount(amount) {
+    Console.print(this.OUTPUT_MESSAGE.AFTER_DISCOUNT_TOTAL_AMOUNT(amount.toLocaleString('ko-KR')));
+  },
+
+  printBadge(badge) {
+    Console.print(this.OUTPUT_MESSAGE.EVENT_BADGE(badge));
   },
 };
 
